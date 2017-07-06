b71b909526af15f9b19b9a268812017baea0e09f
wisnu bhakti <wisnubhakti@indomaret.co.id>
2017-07-06 07:04:11 +0700

    Initial Commit

---
+++ Binar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Binar
+//
+//  Created by Software Development 7 - Mac 2 on 7/6/17.
+//  Copyright © 2017 Software Development 7 - Mac 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
